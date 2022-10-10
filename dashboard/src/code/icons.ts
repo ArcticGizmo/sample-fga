5ad6869871c6ff8fe76736d512a40923a4920471
Jonathan Howell <jon.howell314@gmail.com>
2022-10-10 11:34:12 +0800

    added dashboard

---
+++ dashboard/src/code/icons.ts
@@ -0,0 +1,12 @@
+// find them here
+// https://fontawesome.com/search?s=solid
+export {
+  faHouse,
+  faHatWizard,
+  faCancel,
+  faList,
+  faGamepad,
+  faCheckCircle,
+  faCheck,
+  faXmark
+} from '@fortawesome/free-solid-svg-icons';
